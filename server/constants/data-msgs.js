bb5670f96c90897b89eec762c56d0fc7f70d0e19
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-03-09 14:13:29 +0200

    Server constants for data-msgs and movie-types added.

---
+++ server/constants/data-msgs.js
@@ -0,0 +1,16 @@
+const msgs = {
+    success: {
+        USER_REGISTERED: 'User successfully registered',
+        USER_LOGGEDIN: 'User successfully logged in',
+        LOGOUT: 'Logout successfull'
+    },
+    errors: {
+        REGISTER_FAILED: 'Failed to register user',
+        USER_DOES_NOT_EXIST: 'User does not exist',
+        INVALID_PASS: 'Invalid password',
+        LOGIN_FAILED: 'Failed to log in user',
+        SOMETHING_WENT_WRONG: 'Something went wrong'
+    }
+};
+
+module.exports = msgs;
---
+++ server/constants/movie-types.js
@@ -0,0 +1,6 @@
+const movieTypes = {
+    ORIGINAL_TRILOGY: 0,
+    PREQUEL_TRILOGY: 1,
+    SEQUEL_TRILOGY: 2,
+    STANDALONE: 3
+};
