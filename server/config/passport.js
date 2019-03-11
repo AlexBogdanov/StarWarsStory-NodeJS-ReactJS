98d07758d590373120336bcc2fba6adf490ce732
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-03-11 16:51:08 +0200

    Server passport to append user to request logic added.

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,14 +2,26 @@ const passport = require('passport');
 const LocalPassport = require('passport-local');
 const User = require('mongoose').model('User');
 
+const localStrategy = new LocalPassport((username, password, done) => {
+    User.findOne({ username: username }).then(user => {
+        if (!user) {
+            return done(null, false); 
+        }
+
+        if (!user.authenticate(password)) {
+            return done(null, false);
+        }
+        
+        return done(null, user);
+    });
+});
+
+const strategyNames = {
+    LOCAL: 'local'
+};
+
 module.exports = () => {
-    passport.use(new LocalPassport((username, password, done) => {
-        User.findOne({ username: username }).then(user => {
-            if (!user) return done(null, false);
-            if (!user.authenticate(password)) return done(null, false);
-            return done(null, user);
-        });
-    }));
+    passport.use(strategyNames.LOCAL, localStrategy);
 
     passport.serializeUser((user, done) => {
         if (user) return done(null, user._id);
--- server/routers/user-router.js
+++ server/routers/user-router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 const { userController } = require('./../controllers');
 
 router
     .post('/register', userController.register)
-    .post('/login', userController.login)
+    .post('/login', passport.authenticate('local') , userController.login)
     .post('/logout', userController.logout);
 
 module.exports = router;
