5e34589ff3f5653c09f50e378dce39e3d65db7e0
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-03-24 11:16:30 +0200

    Old server deleted.

--- oldServer/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    development: {
-        port: process.env.PORT || 8080,
-        dbPath: 'mongodb://localhost:27017/star-wars-story-db'
-    },
-    production: {}
-};
--- oldServer/constants/movie-types.js
+++
@@ -1,6 +0,0 @@
-const movieTypes = {
-    ORIGINAL_TRILOGY: 0,
-    PREQUEL_TRILOGY: 1,
-    SEQUEL_TRILOGY: 2,
-    STANDALONE: 3
-};
