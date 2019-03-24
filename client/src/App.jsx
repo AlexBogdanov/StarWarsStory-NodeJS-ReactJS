a6eca93c496079321bc469bb60f3067b823dbdea
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-03-24 19:04:24 +0200

    Client App.jsx and Navbar.jsx updated.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import 'react-notifications/lib/notifications.css';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 import Navbar from './components/navbar/Navbar';
-import { successNotifs } from './constants/notification-messages';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.setUser = this.setUser.bind(this);
-    this.logout = this.logout.bind(this);
-  }
-
-  setUser(user) {
-    localStorage.setItem('userId', user.id);
-    localStorage.setItem('username', user.username);
-    localStorage.setItem('role', user.roles.includes('Admin') ? 'Admin' : 'User');
-  }
-
-  logout() {
-    localStorage.clear();
-    NotificationManager.success(successNotifs.LOGOUT_SUCCESSFULL);
-    setTimeout(() => window.location.href = '/', 1500);
-  }
 
   render() {
     return (
       <div className="App">
-        <Navbar setUser={this.setUser} logout={this.logout} />
+        <Navbar />
         <NotificationContainer />
+
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+        </Switch>
       </div>
     );
   }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,156 +1,39 @@
 import React, { Fragment } from 'react';
-import './Navbar.css'
-import { Switch, Route, Link } from 'react-router-dom';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
-import Register from './../register/Register';
-import Login from './../login/Login';
+import { userRoles } from './../../constants/common';
 
-import CharactersList from './../character/characters-list/CharactersList';
-import CharacterDetails from './../character/character-details/CharacterDetails';
-import CharacterEdit from './../character/character-edit/CharacterEdit';
-import CharacterCreate from './../character/character-create/CharacterCreate';
+const Navbar = () => {
+    const logout = () => {
+        localStorage.clear();
+        sessionStorage.clear();
+        window.location.href = '/';
+    };
 
-import WeaponsList from './../weapon/weapons-list/WeaponsList';
-import WeaponDetails from './../weapon/weapon-details/WeaponDetails';
-import WeaponEdit from './../weapon/weapon-edit/WeaponEdit';
-import WeaponCreate from './../weapon/weapon-create/WeaponCreate';
-
-import SpaceshipsList from './../spaceship/spaceships-list/SpaceshipsList';
-import SpaceshipDetails from './../spaceship/spaceship-details/SpaceshipDetails';
-import SpaceshipEdit from './../spaceship/spaceship-edit/SpaceshipEdit';
-import SpaceshipCreate from './../spaceship/spaceship-create/SpaceshipCreate';
-
-import PlanetsList from './../planet/planets-list/PlanetsList';
-import PlanetDetails from './../planet/planet-details/PlanetDetails';
-import PlanetEdit from './../planet/planet-edit/PlanetEdit';
-import PlanetCreate from './../planet/planet-create/PlanetCreate';
-
-import StoriesList from './../story/stories-list/StoriesList';
-import MoviesList from './../movie/movies-list/MoviesList';
-
-const Navbar = ({ setUser, logout }) => {
-    const userRole = localStorage.getItem('role');
-
-    if (userRole && userRole === 'Admin') {
-        return (
-            <div className="Navbar">
+    const userRole = localStorage.getItem('userRole')
+        ? localStorage.getItem('userRole') : sessionStorage.getItem('userRole');
+        
+    return (
+        <div className="Navbar">
+            {
+                userRole === userRoles.ADMIN ?
                 <Fragment>
-                    <Link to="/logout">Logout</Link>
-                    <br />
-                    <Link to="/characters">characters</Link> <Link to="/character/create">create character</Link>
-                    <br />
-                    <Link to="/weapons">weapons</Link> <Link to="/weapon/create">create weapon</Link>
-                    <br />
-                    <Link to="/spaceships">spaceships</Link> <Link to="/spaceship/create">create spaceship</Link>
-                    <br />
-                    <Link to="/planets">planets</Link> <Link to="/planet/create">create planet</Link>
-                    <br />
-                    <Link to="/stories">stories</Link>
-                    <br />
-                    <Link to="/movies">movies</Link>
+                    <button onClick={logout}>Logout</button>
                 </Fragment>
-
-                {/* Character */}
-                <Route path="/characters" component={CharactersList} />
-                <Route path="/character/details/:characterId" component={CharacterDetails} />
-                <Route path="/character/edit/:characterId" component={CharacterEdit} />
-                <Route path="/character/create" component={CharacterCreate} />
-
-                {/* Weapon */}
-                <Route path="/weapons" component={WeaponsList} />
-                <Route path="/weapon/details/:weaponId" component={WeaponDetails} />
-                <Route path="/weapon/edit/:weaponId" component={WeaponEdit} />
-                <Route path="/weapon/create" component={WeaponCreate} />
-
-                {/* Spaceship */}
-                <Route path="/spaceships" component={SpaceshipsList} />
-                <Route path="/spaceship/details/:spaceshipId" component={SpaceshipDetails} />
-                <Route path="/spaceship/edit/:spaceshipId" component={SpaceshipEdit} />
-                <Route path="/spaceship/create" component={SpaceshipCreate} />
-
-                {/* Planet */}
-                <Route path="/planets" component={PlanetsList} />
-                <Route path="/planet/details/:planetId" component={PlanetDetails} />
-                <Route path="/planet/edit/:planetId" component={PlanetEdit} />
-                <Route path="/planet/create" component={PlanetCreate} />
-
-                <Route path="/stories" component={StoriesList} />
-                <Route path="/movies" component={MoviesList} />
-            </div>
-        );
-    } else if (userRole && userRole === 'User') {
-        return (
-            <div className="Navbar">
+                : userRole === userRoles.USER ?
                 <Fragment>
-                    <Link to="/logout">Logout</Link>
-                    <br />
-                    <Link to="/characters">characters</Link>
-                    <br />
-                    <Link to="/weapons">weapons</Link>
-                    <br />
-                    <Link to="/spaceships">spaceships</Link>
-                    <br />
-                    <Link to="/planets">planets</Link>
-                    <br />
-                    <Link to="/stories">stories</Link>
-                    <br />
-                    <Link to="/movies">movies</Link>
+                    <button onClick={logout}>Logout</button>
                 </Fragment>
-
-                {/* Character */}
-                <Route path="/characters" component={CharactersList} />
-                <Route path="/character/details/:characterId" component={CharacterDetails} />
-
-                {/* Weapon */}
-                <Route path="/weapons" component={WeaponsList} />
-                <Route path="/weapon/details/:weaponId" component={WeaponDetails} />
-
-                {/* Spaceship */}
-                <Route path="/spaceships" component={SpaceshipsList} />
-                <Route path="/spaceship/details/:spaceshipId" component={SpaceshipDetails} />
-
-                {/* Planet */}
-                <Route path="/planets" component={PlanetsList} />
-                <Route path="/planet/details/:planetId" component={PlanetDetails} />
-
-                <Route path="/stories" component={StoriesList} />
-                <Route path="/movies" component={MoviesList} />
-            </div>
-        );
-    } else {
-        return (
-            <div className="Navbar">
+                :
                 <Fragment>
-                    <Link to="/login">login</Link>
+                    <Link to="/register">Register</Link>
                     <br />
-                    <Link to="/register">register</Link>
-                    <br />
-                    <Link to="/characters">characters</Link>
-                    <br />
-                    <Link to="/weapons">weapons</Link>
-                    <br />
-                    <Link to="/spaceships">spaceships</Link>
-                    <br />
-                    <Link to="/planets">planets</Link>
-                    <br />
-                    <Link to="/stories">stories</Link>
-                    <br />
-                    <Link to="/movies">movies</Link>
+                    <Link to="/login">Login</Link>
                 </Fragment>
-
-                <Switch>
-                    <Route path="/register" component={Register} />
-                    <Route path="/login" render={() => <Login setUser={setUser} />} />
-                    <Route path="/characters" component={CharactersList} />
-                    <Route path="/weapons" component={WeaponsList} />
-                    <Route path="/spaceships" component={SpaceshipsList} />
-                    <Route path="/planets" component={PlanetsList} />
-                    <Route path="/stories" component={StoriesList} />
-                    <Route path="/movies" component={MoviesList} />
-                </Switch>
-            </div>
+            }
+        </div>
         );
-    }
 };
 
 export default Navbar;
