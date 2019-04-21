6bdaa8a29979ca2c6aabcc1e16f69713862c4563
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-04-21 17:40:56 +0300

    Client routes updated.

--- client/src/routes/authUser-routes.js
+++ client/src/routes/authUser-routes.js
@@ -1,39 +1,39 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './../components/home/Home';
+import Home from '../components/home/Home';
 
 // Character
-import CharacterList from './../components/character/characters-list/CharactersList';
-import CharacterDetails from './../components/character/character-details/CharacterDetails';
-import CharacterCreate from './../components/character/character-create/CharacterCreate';
-import CharacterEdit from './../components/character/character-edit/CharacterEdit';
+import CharacterList from '../components/character/characters-list/CharactersList';
+import CharacterDetails from '../components/character/character-details/CharacterDetails';
+import CharacterCreate from '../components/character/character-create/CharacterCreate';
+import CharacterEdit from '../components/character/character-edit/CharacterEdit';
 
 // Weapon
-import WeaponList from './../components/weapon/weapons-list/WeaponsList';
-import WeaponDetails from './../components/weapon/weapon-details/WeaponDetails';
-import WeaponCreate from './../components/weapon/weapon-create/WeaponCreate';
-import WeaponEdit from './../components/weapon/weapon-edit/WeaponEdit';
+import WeaponList from '../components/weapon/weapons-list/WeaponsList';
+import WeaponDetails from '../components/weapon/weapon-details/WeaponDetails';
+import WeaponCreate from '../components/weapon/weapon-create/WeaponCreate';
+import WeaponEdit from '../components/weapon/weapon-edit/WeaponEdit';
 
 // Planet
-import PlanetList from './../components/planet/planets-list/PlanetsList';
-import PlanetDetails from './../components/planet/planet-details/PlanetDetails';
-import PlanetCreate from './../components/planet/planet-create/PlanetCreate';
-import PlanetEdit from './../components/planet/planet-edit/PlanetEdit';
+import PlanetList from '../components/planet/planets-list/PlanetsList';
+import PlanetDetails from '../components/planet/planet-details/PlanetDetails';
+import PlanetCreate from '../components/planet/planet-create/PlanetCreate';
+import PlanetEdit from '../components/planet/planet-edit/PlanetEdit';
 
 // Spaceship
-import SpaceshipList from './../components/spaceship/spaceships-list/SpaceshipsList';
-import SpaceshipDetails from './../components/spaceship/spaceship-details/SpaceshipDetails';
-import SpaceshipCreate from './../components/spaceship/spaceship-create/SpaceshipCreate';
-import SpaceshipEdit from './../components/spaceship/spaceship-edit/SpaceshipEdit';
+import SpaceshipList from '../components/spaceship/spaceships-list/SpaceshipsList';
+import SpaceshipDetails from '../components/spaceship/spaceship-details/SpaceshipDetails';
+import SpaceshipCreate from '../components/spaceship/spaceship-create/SpaceshipCreate';
+import SpaceshipEdit from '../components/spaceship/spaceship-edit/SpaceshipEdit';
 
 // Movie
-import MovieList from './../components/movie/movies-list/MoviesList';
-import MovieDetails from './../components/movie/movie-details/MovieDetails';
-import MovieCreate from './../components/movie/movie-create/MovieCreate';
-import MovieEdit from './../components/movie/movie-edit/MovieEdit';
+import MovieList from '../components/movie/movies-list/MoviesList';
+import MovieDetails from '../components/movie/movie-details/MovieDetails';
+import MovieCreate from '../components/movie/movie-create/MovieCreate';
+import MovieEdit from '../components/movie/movie-edit/MovieEdit';
 
-const AdminRoutes = (props) => {
+const AuthRoutes = (props) => {
     return (
         <Fragment>
             <CharacterCreate notifHandler={props.notifHandler} isOpen={props.openCharacterCreate} toggle={props.toggleCharacter} />
@@ -73,4 +73,4 @@ const AdminRoutes = (props) => {
     );
 };
 
-export default AdminRoutes;
+export default AuthRoutes;
